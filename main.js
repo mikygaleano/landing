5c3bdef7384590937158b2173148c1df4a0426ef
mikygaleano <creatormikeonline@gmail.com>
2023-07-27 20:42:16 -0300

    noveno commit

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 import 'tailwindcss/tailwind.css';
 
 
+function particles (body) {
+    for (let i = 0; i < 100; i++) {
+        let particulas = document.createElement('div');
+        particulas.classList.add('particle');
+        document.querySelector(body).append(particulas);
+    };
+    
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    particles('section')
+})
